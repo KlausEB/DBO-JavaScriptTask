e5ffc33a8cd9f20df4179cedd2dbe331d14360e4
Nikolai Polevoi <nikolai_polevoi@epam.com>
2021-12-07 02:55:05 +0300

    EPMRDRUTVE-1237 All done

---
+++ my-statement-data.js
@@ -0,0 +1,504 @@
+const myStatementData = [
+    {
+        _id: '5ae17f666641d44ec7d772e1',
+        date: '2015-02-12T06:40:26',
+        type: 'Кредит',
+        amount: -3495.31,
+    },
+    {
+        _id: '5ae17f668f6e31eeccbad86b',
+        date: '2016-12-22T09:40:25',
+        type: 'Перевод',
+        amount: -56.06,
+    },
+    {
+        _id: '5ae17f66c0cbe4aba08dcf4a',
+        date: '2018-03-31T07:14:44',
+        type: 'Перевод',
+        amount: 2590,
+    },
+    {
+        _id: '5ae17f6629d2125fead49e6d',
+        date: '2015-08-05T08:41:31',
+        type: 'Перевод',
+        amount: 43388.28,
+    },
+    {
+        _id: '5ae17f66c56ae1efed72f85a',
+        date: '2016-09-26T12:05:30',
+        type: 'Перевод',
+        amount: 83769.05,
+    },
+    {
+        _id: '5ae17f66e68d29a7fc96fe9d',
+        date: '2016-02-18T08:12:25',
+        type: 'Начисление процентов',
+        amount: 23320.15,
+    },
+    {
+        _id: '5ae17f6657f62ef3b1745695',
+        date: '2015-03-21T05:59:35',
+        type: 'Магазины',
+        amount: 20111.3,
+    },
+    {
+        _id: '5ae17f66eb33ec3d20fd2fae',
+        date: '2017-01-02T07:51:58',
+        type: 'Кредит',
+        amount: 13878.56,
+    },
+    {
+        _id: '5ae17f668a5410c3583fd77a',
+        date: '2014-09-09T01:39:11',
+        type: 'Перевод',
+        amount: 76948.45,
+    },
+    {
+        _id: '5ae17f668a2f2f36398c369c',
+        date: '2016-11-09T04:02:18',
+        type: 'Кредит',
+        amount: 31158.44,
+    },
+    {
+        _id: '5ae17f6687b1f72ce2bfad3d',
+        date: '2017-01-02T05:57:23',
+        type: 'Магазины',
+        amount: 59593.8,
+    },
+    {
+        _id: '5ae17f66ae9dc1477ff6300f',
+        date: '2015-04-27T09:01:44',
+        type: 'Кредит',
+        amount: 83519.88,
+    },
+    {
+        _id: '5ae17f66db15f5a7da4c7584',
+        date: '2015-02-19T01:55:37',
+        type: 'Магазины',
+        amount: 18049.58,
+    },
+    {
+        _id: '5ae17f664665a2ebf292e752',
+        date: '2016-09-26T09:02:12',
+        type: 'Перевод',
+        amount: 51779.86,
+    },
+    {
+        _id: '5ae17f665aa43385a3d52475',
+        date: '2014-12-18T08:09:29',
+        type: 'Начисление процентов',
+        amount: 83511.73,
+    },
+    {
+        _id: '5ae17f66c1b9ed550fd60812',
+        date: '2017-02-20T05:58:47',
+        type: 'Кредит',
+        amount: 83292.78,
+    },
+    {
+        _id: '5ae17f662c7164918520db88',
+        date: '2017-05-26T07:19:42',
+        type: 'Кредит',
+        amount: -4475.79,
+    },
+    {
+        _id: '5ae17f66856871ac8af7de67',
+        date: '2017-09-02T12:37:39',
+        type: 'Начисление процентов',
+        amount: 12165.78,
+    },
+    {
+        _id: '5ae17f66b4ea99224a09d7cb',
+        date: '2017-02-08T01:15:06',
+        type: 'Кредит',
+        amount: 8388.97,
+    },
+    {
+        _id: '5ae17f66217a73e4b4924055',
+        date: '2017-09-15T12:48:26',
+        type: 'Перевод',
+        amount: 90960.39,
+    },
+    {
+        _id: '5ae17f6661b9a3528db11b9d',
+        date: '2015-04-19T01:23:55',
+        type: 'Магазины',
+        amount: 55039.33,
+    },
+    {
+        _id: '5ae17f6657beacdd4e24f726',
+        date: '2016-08-26T02:30:16',
+        type: 'Начисление процентов',
+        amount: 59616.09,
+    },
+    {
+        _id: '5ae17f66a82fe0a4980a0dc3',
+        date: '2017-03-06T11:49:01',
+        type: 'Кредит',
+        amount: 1786.82,
+    },
+    {
+        _id: '5ae17f66c649906f23644214',
+        date: '2017-03-19T01:35:43',
+        type: 'Кредит',
+        amount: 83842.55,
+    },
+    {
+        _id: '5ae17f665469da535f874774',
+        date: '2017-03-26T04:45:48',
+        type: 'Магазины',
+        amount: 14347.64,
+    },
+    {
+        _id: '5ae17f66dcb5349dea2b456d',
+        date: '2016-02-17T09:24:02',
+        type: 'Перевод',
+        amount: 24817.82,
+    },
+    {
+        _id: '5ae17f66eafbf2d367247ef7',
+        date: '2015-10-11T03:23:55',
+        type: 'Кредит',
+        amount: 97439.9,
+    },
+    {
+        _id: '5ae17f6618baa8e46746a366',
+        date: '2018-04-24T08:28:54',
+        type: 'Перевод',
+        amount: -7430.31,
+    },
+    {
+        _id: '5ae17f66c32acd0ebe4f100c',
+        date: '2015-04-18T08:20:40',
+        type: 'Начисление процентов',
+        amount: 20487.86,
+    },
+    {
+        _id: '5ae17f668920bc8ee05e5558',
+        date: '2017-04-20T09:09:45',
+        type: 'Начисление процентов',
+        amount: 41407.38,
+    },
+    {
+        _id: '5ae17f66c53131c4de422594',
+        date: '2015-11-22T12:52:39',
+        type: 'Кредит',
+        amount: 88014.82,
+    },
+    {
+        _id: '5ae17f66d617ff9149c68c55',
+        date: '2016-11-01T10:25:55',
+        type: 'Кредит',
+        amount: 35947.58,
+    },
+    {
+        _id: '5ae17f6668db5715941c8835',
+        date: '2015-09-22T06:20:16',
+        type: 'Кредит',
+        amount: -5301.11,
+    },
+    {
+        _id: '5ae17f6640c5a0e23a075d2f',
+        date: '2016-09-06T06:05:16',
+        type: 'Магазины',
+        amount: 98331.39,
+    },
+    {
+        _id: '5ae17f662a9ff83bf08f2095',
+        date: '2014-08-23T06:48:55',
+        type: 'Магазины',
+        amount: 38431.21,
+    },
+    {
+        _id: '5ae17f66c8b94bbbf09fb027',
+        date: '2016-08-22T05:34:03',
+        type: 'Начисление процентов',
+        amount: 69438.38,
+    },
+    {
+        _id: '5ae17f6668ebe845b3980d97',
+        date: '2017-10-29T02:36:58',
+        type: 'Кредит',
+        amount: 77305.27,
+    },
+    {
+        _id: '5ae17f66f6655d3e438a60b5',
+        date: '2015-02-18T08:18:38',
+        type: 'Кредит',
+        amount: 18087.04,
+    },
+    {
+        _id: '5ae17f66d4f67524947ced5d',
+        date: '2017-03-09T04:46:24',
+        type: 'Начисление процентов',
+        amount: 74078.78,
+    },
+    {
+        _id: '5ae17f66a3fb7a335450e076',
+        date: '2016-03-04T09:51:48',
+        type: 'Перевод',
+        amount: -3455.41,
+    },
+    {
+        _id: '5ae17f66eef06ea06fd9e250',
+        date: '2014-06-03T12:58:53',
+        type: 'Начисление процентов',
+        amount: 7512.29,
+    },
+    {
+        _id: '5ae17f66996d95c7420a4938',
+        date: '2016-04-18T05:06:07',
+        type: 'Магазины',
+        amount: 63408.43,
+    },
+    {
+        _id: '5ae17f664a399af51daaa9a2',
+        date: '2014-10-08T04:50:08',
+        type: 'Магазины',
+        amount: -1751.87,
+    },
+    {
+        _id: '5ae17f66d06233db53d8dee2',
+        date: '2015-03-26T01:56:05',
+        type: 'Кредит',
+        amount: 90868.74,
+    },
+    {
+        _id: '5ae17f661bca1bd742ada082',
+        date: '2016-04-02T01:04:02',
+        type: 'Магазины',
+        amount: -3064.64,
+    },
+    {
+        _id: '5ae17f66ebe658ba0b45961b',
+        date: '2014-02-02T09:43:50',
+        type: 'Начисление процентов',
+        amount: 20437.17,
+    },
+    {
+        _id: '5ae17f66bcb7b1b152555a7c',
+        date: '2014-02-14T11:47:34',
+        type: 'Кредит',
+        amount: 43544.84,
+    },
+    {
+        _id: '5ae17f661ada73ba8d30b51b',
+        date: '2014-05-05T08:48:40',
+        type: 'Начисление процентов',
+        amount: -3219.46,
+    },
+    {
+        _id: '5ae17f66c6064f4a0616dcba',
+        date: '2017-08-18T12:05:09',
+        type: 'Магазины',
+        amount: -8006.73,
+    },
+    {
+        _id: '5ae17f66e5932afad62787db',
+        date: '2016-05-31T08:29:26',
+        type: 'Перевод',
+        amount: 89428.83,
+    },
+];
+const tableId = ".datasource-scroll-table";
+const dateCheckbox = document.querySelector("#date-column-checkbox");
+const timeCheckbox = document.querySelector("#time-column-checkbox");
+const typeCheckbox = document.querySelector("#type-column-checkbox");
+const profitCheckbox = document.querySelector("#profit-column-checkbox");
+const expensesCheckbox = document.querySelector("#expenses-column-checkbox");
+const checkboxes = [dateCheckbox, timeCheckbox, typeCheckbox, profitCheckbox, expensesCheckbox];
+const groupSelector = document.querySelector('#group-select');
+const nonGrouping = document.querySelector('#non');
+const dateGrouping = document.querySelector('#date');
+let activeCheckboxCounter = 0;
+
+myStatementData.sort(function (a, b) {
+    return Date.parse(a.date) - Date.parse(b.date);
+});
+
+function writeRowStatement(table, date, time, typeOperation, profit, expenses) {
+    let row = document.createElement("TR");
+    let dateColumn = document.createElement("TD");
+    let timeColumn = document.createElement("TD");
+    let typeColumn = document.createElement("TD");
+    let profitColumn = document.createElement("TD");
+    let expensesColumn = document.createElement("TD");
+
+    dateColumn.setAttribute('class', 'date-column');
+    timeColumn.setAttribute('class', 'time-column');
+    typeColumn.setAttribute('class', 'type-column');
+    profitColumn.setAttribute('class', 'profit-column');
+    expensesColumn.setAttribute('class', 'expenses-column');
+
+    dateColumn.appendChild(document.createTextNode(date));
+    timeColumn.appendChild(document.createTextNode(time));
+    typeColumn.appendChild(document.createTextNode(typeOperation));
+
+    if (profit !== null && profit !== 0) {
+        profitColumn.appendChild(document.createTextNode('+' + profit));
+    }
+    if (expenses !== null && expenses !== 0) {
+        expensesColumn.appendChild(document.createTextNode(expenses));
+    }
+    row.appendChild(dateColumn);
+    row.appendChild(timeColumn);
+    row.appendChild(typeColumn);
+    row.appendChild(profitColumn);
+    row.appendChild(expensesColumn);
+    table.appendChild(row);
+}
+
+function printFullDateStatement(tableId) {
+    groupByDateCheckboxMod(false);
+    removeOldTable(tableId);
+    let table = document.querySelector(tableId).querySelector('tbody');
+    myStatementData.forEach(function (elem) {
+        const elemDateTime = new Date(elem.date);
+        const elemDate = elemDateTime.toLocaleDateString();
+        const elemTime = elemDateTime.toLocaleTimeString();
+        const elemType = elem.type;
+        const elemMoney = Number(elem.amount);
+        const localeStringMoney = elemMoney.toLocaleString('default', {style: 'currency', currency: 'RUB'});
+        if (elemMoney > 0) {
+            writeRowStatement(table, elemDate, elemTime, elemType, localeStringMoney, null);
+        } else if (elemMoney < 0) {
+            writeRowStatement(table, elemDate, elemTime, elemType, null, localeStringMoney);
+        } else {
+            writeRowStatement(table, elemDate, elemTime, elemType, null, null);
+        }
+    });
+    updateVisibility();
+}
+
+function changeColumnVisibility(checkbox) {
+    let columnClass;
+    switch (checkbox) {
+        case dateCheckbox:
+            columnClass = '.date-column';
+            break;
+        case timeCheckbox:
+            columnClass = '.time-column';
+            break;
+        case typeCheckbox:
+            columnClass = '.type-column';
+            break;
+        case profitCheckbox:
+            columnClass = '.profit-column';
+            break;
+        case expensesCheckbox:
+            columnClass = '.expenses-column';
+            break;
+    }
+    let columnArray = document.querySelectorAll(columnClass);
+    if (checkbox.checked) {
+        activeCheckboxCounter++;
+        columnArray.forEach(function (elem) {
+            elem.style.display = "";
+        });
+    } else {
+        activeCheckboxCounter--;
+        columnArray.forEach(function (elem) {
+            elem.style.display = "none";
+        });
+    }
+}
+
+function updateVisibility() {
+    checkboxes.forEach(function (elem) {
+        changeColumnVisibility(elem);
+    });
+}
+
+dateCheckbox.addEventListener('change', () => {
+    changeColumnVisibility(dateCheckbox);
+    checkCheckboxesForBlocking();
+})
+
+timeCheckbox.addEventListener('change', () => {
+    changeColumnVisibility(timeCheckbox);
+    checkCheckboxesForBlocking();
+})
+typeCheckbox.addEventListener('change', () => {
+    changeColumnVisibility(typeCheckbox);
+    checkCheckboxesForBlocking();
+})
+profitCheckbox.addEventListener('change', () => {
+    changeColumnVisibility(profitCheckbox);
+    checkCheckboxesForBlocking();
+})
+expensesCheckbox.addEventListener('change', () => {
+    changeColumnVisibility(expensesCheckbox);
+    checkCheckboxesForBlocking();
+})
+
+function checkCheckboxesForBlocking() {
+    if (activeCheckboxCounter === 1) {
+        checkboxes.forEach(function (elem) {
+            if (elem.checked) {
+                elem.disabled = true;
+            }
+        });
+    } else {
+        checkboxes.forEach(function (elem) {
+            elem.disabled = false;
+        });
+    }
+}
+
+function groupByDate(tableId) {
+    groupByDateCheckboxMod(true);
+    removeOldTable(tableId);
+    let table = document.querySelector(tableId).querySelector('tbody');
+    let pastDate = null;
+    let profitSum = 0;
+    let expensesSum = 0;
+    myStatementData.forEach(function (elem) {
+        const currentDate = new Date(elem.date).toLocaleDateString();
+        const elemMoney = Number(elem.amount);
+        const currentProfit = elemMoney > 0 ? elemMoney : 0;
+        const currentExpenses = elemMoney < 0 ? elemMoney : 0;
+        if (pastDate === null) {
+            pastDate = currentDate;
+        } else if (pastDate !== new Date(elem.date).toLocaleDateString()) {
+            writeRowStatement(table, pastDate, null, null, profitSum, expensesSum);
+            pastDate = new Date(elem.date).toLocaleDateString()
+            profitSum = 0;
+            expensesSum = 0;
+        }
+        profitSum += currentProfit;
+        expensesSum += currentExpenses;
+    });
+    updateVisibility();
+}
+
+function groupByDateCheckboxMod(isGroupByDateMod) {
+    activeCheckboxCounter = 0;
+    if (isGroupByDateMod) {
+        checkboxes.forEach(function (elem) {
+            elem.checked = elem !== timeCheckbox && elem !== typeCheckbox;
+            elem.disabled = true;
+        });
+    } else {
+        checkboxes.forEach(function (elem) {
+            elem.checked = true;
+            elem.disabled = false;
+        });
+    }
+}
+
+function removeOldTable(tableId) {
+    let oldTable = document.querySelector(tableId).querySelector('tbody');
+    let newTable = document.createElement('tbody');
+    oldTable.parentNode.replaceChild(newTable, oldTable);
+}
+
+groupSelector.addEventListener('change', () => {
+    if (nonGrouping.selected) {
+        printFullDateStatement(tableId);
+    }
+    if (dateGrouping.selected) {
+        groupByDate(tableId);
+    }
+});
+
+printFullDateStatement(tableId);
